e832fc1e3ec0c1f9c8860770b186836c5d45618c
gizmostudios <bert@gizmostudios.net>
2019-03-27 20:02:16 +0100

    Added carousel to homepage

--- except_wagtail/home/models.py
+++ except_wagtail/home/models.py
@@ -1,9 +1,27 @@
 from django.db import models
 
-from wagtail.core.models import Page
+from modelcluster.fields import ParentalKey
+
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
 from wagtail.images.edit_handlers import ImageChooserPanel
+
+class CarouselItem(Orderable):
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    caption = models.CharField(max_length=255, blank=True)
+    page = ParentalKey('HomePage', related_name='carousel_items')
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('caption'),
+    ]
 
 class HomePage(Page):
 
@@ -17,4 +35,5 @@ class HomePage(Page):
         FieldPanel('hero_title'),
         FieldPanel('hero_subtitle'),
         FieldPanel('body', classname="full"),
+        InlinePanel('carousel_items', label="Carousel Items"),
     ]
